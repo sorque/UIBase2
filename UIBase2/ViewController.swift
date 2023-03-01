d71e84824b2db2fec771a60b9fec709774161319
Sorque <michvl@yandex.ru>
2023-03-01 23:23:23 +0300

    Initial Commit

---
+++ UIBase2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIBase2
+//
+//  Created by m.korovin on 01.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
